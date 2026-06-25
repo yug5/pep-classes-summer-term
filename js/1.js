2edb165678a73cf283bce996bc45b461e7b268d9
UD <yugshrimali82@gmail.com>
2026-06-25 10:41:19 +0530

    2nd week

---
+++ js/1.js
@@ -0,0 +1,23 @@
+// function sayHello() {
+//   var n = 5;
+//   if (true) {
+//     n = 7;
+//     console.log(n);
+//   }
+//   console.log(n);
+// }
+// sayHello();
+// var n = 8;
+// function scope() {
+//   let n = 5;
+//   if (true) {
+//     let n = 7;
+//     console.log(n);
+//   }
+//   console.log(n);
+// }
+// console.log(n);
+// scope();
+
+let n = 8;
+console.log(n);
